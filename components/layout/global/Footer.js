d27eef4405316b568acde9e9ba384e1b94df76d2
Marius Hesby <marius.r.hesby@gmail.com>
2022-06-12 23:19:12 +0200

    Update Footer.js

--- components/layout/global/Footer.js
+++ components/layout/global/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
 
     return (
         <footer className="text-center border-t border-gray-300 py-5">
-            <em>mariushesby.no @ 2022</em>
+            <em>mariushesby.no @ 2022 | My <a className="text-blue-700 hover:scale-125" href="https://github.com/MariusHesby">GitHub page</a></em>
         </footer>
     );
 }
