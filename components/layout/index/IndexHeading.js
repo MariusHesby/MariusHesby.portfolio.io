730cb169141c97cf0938320a05d68e5f14721803
Marius Hesby <marius.r.hesby@gmail.com>
2022-06-12 22:26:09 +0200

    Edited the heading

--- components/layout/index/IndexHeading.js
+++ components/layout/index/IndexHeading.js
@@ -5,7 +5,7 @@ export default function IndexHeading() {
   return (
     <div className="h-screen px-5 sm:px-10 grid content-center relative">
       <ProfileImg />
-      <h1>I create<br /><span>simple </span>and <span>solid</span><br /><span>websites</span><br />that work!</h1>
+      <h1>I create<br /><span>fast, </span><span>simple </span>and<span><br />solid </span>websites<br />- that work!</h1>
       <Nav />
     </div>
   );
