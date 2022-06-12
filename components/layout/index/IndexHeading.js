33ed8d19a96910daa400123bd075783607ddefbe
Marius Hesby <marius.r.hesby@gmail.com>
2022-06-12 22:52:36 +0200

    Update IndexHeading.js

--- components/layout/index/IndexHeading.js
+++ components/layout/index/IndexHeading.js
@@ -6,7 +6,7 @@ export default function IndexHeading() {
     <div className="h-screen px-5 sm:px-10 grid content-center relative">
       <ProfileImg />
       <h1>I create<br /><span>fast, </span><span>simple </span>and<span><br />solid </span>websites<br />- that work!</h1>
-      <p>Remember to check out my <a href="https://github.com/MariusHesby">GitHub page</a></p>
+      <p className="mx-auto">Don't forget to check out my <a className="text-blue-700 hover:scale-125" href="https://github.com/MariusHesby">GitHub page</a></p>
       <Nav />
     </div>
   );
